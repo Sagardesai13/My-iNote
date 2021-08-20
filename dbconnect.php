3a1f969d2b65e60cf3316fe26826e1f53adc443f
Sagar Desai <74409690+Sagardesai13@users.noreply.github.com>
2021-08-20 10:40:51 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -8,6 +8,7 @@ $database = "crud";
 
 // Create a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
+echo $conn;
 
 // Die if connection was not successful
 if (!$conn){
